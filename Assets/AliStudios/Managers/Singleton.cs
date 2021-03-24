5737ceaf80dc466a7d73b4b8c47b81c08007b2b3
Ali Adil <aliadilg@gmail.com>
2021-03-24 17:52:37 +0500

    project organization and code update

---
+++ Assets/AliStudios/Managers/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace alistudios
+{
+    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        // One instance
+        private static T _instance;
+
+        //Public variable which is generic T
+        public static T Instance
+        {
+            get
+            {
+                _instance = (T)FindObjectOfType(typeof(T));
+                if(_instance == null)
+                {
+                    GameObject obj = new GameObject();
+                    _instance = obj.AddComponent<T>();
+                    obj.name  = typeof(T).ToString();
+                
+                }
+                return _instance;
+            }
+
+        }
+    }
+}
