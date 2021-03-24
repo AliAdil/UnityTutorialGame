4f0c57dedbb3ee5795ebe88b8eeecdd8ac7442c0
Ali Adil <aliadilg@gmail.com>
2021-03-24 15:22:17 +0500

    first commit

---
+++ Assets/TestingCube.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace alistudios
+{
+    public class TestingCube : MonoBehaviour
+    {
+        void Update()
+        {
+            if (VirtualInputManager.Instance.MoveLeft && VirtualInputManager.Instance.MoveRight)
+            {
+                return;
+            }
+            if (VirtualInputManager.Instance.MoveRight)
+            {   // for player movement to right
+                this.gameObject.transform.Translate(Vector3.forward * 10f * Time.deltaTime);
+                // turn player face on right
+                // Euler(X,Y,Z) axis
+                this.gameObject.transform.rotation = Quaternion.Euler(0f,0f,0f);
+            }
+
+            if (VirtualInputManager.Instance.MoveLeft)
+            {
+                // for player movement to left 
+                this.gameObject.transform.Translate(Vector3.forward * 10f * Time.deltaTime);
+                // turn player face on left 
+                this.gameObject.transform.rotation = Quaternion.Euler(0f,180f,0f);
+            }
+        }
+
+    }
+}
---
+++ Assets/VirtualInputManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace alistudios
+{
+    public class VirtualInputManager : MonoBehaviour
+    {
+        public static VirtualInputManager Instance = null;
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            else if (Instance != null)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+
+        public bool MoveRight;
+        public bool MoveLeft;
+    }
+}
