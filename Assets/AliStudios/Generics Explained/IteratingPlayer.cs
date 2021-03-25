5959a7e02de58e194719981ec21e407ffec74aaf
Ali Adil <aliadilg@gmail.com>
2021-03-25 17:10:30 +0500

    Practicing Generics

---
+++ Assets/AliStudios/Generics Explained/IteratingPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IterationgPlayer : MonoBehaviour
+{
+    public List<int> intList = new List<int>();
+    public List<string> stringList = new List<string>();
+
+    public void IterateThrough<T>(List<T> targetList)
+    {
+        foreach (T data in targetList)
+        {
+            Debug.Log(data);
+        }
+    }
+}
