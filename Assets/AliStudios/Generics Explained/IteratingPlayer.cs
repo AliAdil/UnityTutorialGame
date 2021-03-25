8d2469780aae509b2ed20e5a9776a9ef760f50a0
Ali Adil <aliadilg@gmail.com>
2021-03-25 20:10:06 +0500

    Update

--- Assets/AliStudios/Generics Explained/IteratingPlayer.cs
+++ Assets/AliStudios/Generics Explained/IteratingPlayer.cs
@@ -1,17 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class IterationgPlayer : MonoBehaviour
+namespace alistudios
 {
-    public List<int> intList = new List<int>();
-    public List<string> stringList = new List<string>();
-
-    public void IterateThrough<T>(List<T> targetList)
+    public class IteratingPlayer : MonoBehaviour
     {
-        foreach (T data in targetList)
+        public List<int> intList = new List<int>();
+        public List<string> stringList = new List<string>();
+
+        public void IterateThrough<T>(List<T> targetList)
         {
-            Debug.Log(data);
+            foreach (T data in targetList)
+            {
+                Debug.Log(data);
+            }
+        }
+
+        private void Start()
+        {
+            IterateThrough(intList);
+            IterateThrough(stringList);
         }
     }
 }
