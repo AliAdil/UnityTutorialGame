305272798d64a68522b32a7f0bceb5becbcd016a
Ali Adil <aliadilg@gmail.com>
2021-03-30 22:56:54 +0500

    Working on ScriptableObjects part1

--- Assets/AliStudios/CharacterAli/States/CharacterStateBase.cs
+++ Assets/AliStudios/CharacterAli/States/CharacterStateBase.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,7 +9,22 @@ namespace alistudios
     // Inherited Character Statemachine in our customClass 
     public class CharacterStateBase : StateMachineBehaviour
     {
-        // Start is called before the first frame update
+
+        public List<StateData> ListAbilityData = new List<StateData>();
+
+        public void UpdateAll(CharacterStateBase characterStateBase, Animator animator)
+        {
+            foreach (StateData d in ListAbilityData)
+            {
+                d.updateAbility(characterStateBase, animator);
+            }
+        }
+
+        public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            UpdateAll(this,animator);
+        }
+
         //Encapsulation 
         // Private variable for characterController Script
         private CharacterController characterController;
@@ -19,9 +35,9 @@ namespace alistudios
             if (characterController == null)
             {
                 characterController = animator.GetComponentInParent<CharacterController>();
-               
+
             }
-           return characterController;
+            return characterController;
         }
     }
 }
---
+++ Assets/StateData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace alistudios
+{
+    public abstract class StateData : ScriptableObject
+    {
+        public float Duration;
+
+        public abstract void updateAbility(CharacterStateBase characterStateBase, Animator animator);
+    }
+}
