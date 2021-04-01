1536321006ad404848c898ba3dad7fbfa54c62a5
Ali Adil <aliadilg@gmail.com>
2021-04-01 22:03:38 +0500

    Update Character Controller

--- Assets/AliStudios/CharacterAli/States/StateScripts/Idle.cs
+++ Assets/AliStudios/CharacterAli/States/StateScripts/Idle.cs
@@ -1,13 +1,18 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace alistudios
 {
+    //for creating menu in unity 
     [CreateAssetMenu(fileName = "New State", menuName = "alistudios/abilityData/Idle")]
+
+    // Idle class extentending stateData which is abstract class extending scriptable object
     public class Idle : StateData
     {
+
+        // overriding StateData Abstract Method
         public override void updateAbility(CharacterState characterState, Animator animator)
         {
              // when Right button is pressed 
